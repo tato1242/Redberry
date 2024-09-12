11f55651d93a6d74d4b6cbc7bca688f72c8e1167
unknown <tatoamiranashvili497@gmail.com>
2024-09-12 17:48:30 +0400

    first push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <div className="border-[1px] border-gray-300  px-[162px] py-[36px] h-[100px] w-[1920px] ">
+          <img  className=" h-[24px] w-[150px] " src="LOGO.png" alt="" />
+          </div>
+        </header>
+        {children}
+        </body>
     </html>
   );
 }
